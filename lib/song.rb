f6f07f0a69bf3ab481743efbf6df7d27927c3889
Jeremy Bourne <jbourne25@yahoo.com>
2019-12-05 19:33:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,8 +43,14 @@ class Song
     sort 
   end 
   
-  def self.new_from_filename
-    
+  def self.new_from_filename(filename)
+     song_array = filename.split(" - ")
+    song_array[1] = song_array[1].chomp(".mp3")
+    song = self.new
+    song.name = song_array[1]
+    song.artist_name = song_array[0]
+    song
+  end
   end 
   
   def 
